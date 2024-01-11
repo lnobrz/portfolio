f48d0a2228c2b378e27970fa37990f80fa9bd537
lnobrz <lucasnobre37@outlook.com>
2024-01-11 19:12:36 -0300

    feat: add portuguese strings

---
+++ data/strings.ts
@@ -0,0 +1,48 @@
+import ExperienceSection from "@/components/home-page/sections/ExperienceSection/ExperienceSection";
+
+const strings = {
+  heroSection: {
+    title: "Soluções digitais personalizadas para seu negócio",
+    subtitle: {
+      saudation: "Oi",
+      ownersPresentation: "Me chamo",
+      ownersName: "Lucas",
+    },
+    paragraph:
+      "Ofereço criação de sites, e-commerces, bots e soluções com inteligência artificial para elevar seu negócio.",
+    ctaButton: "Eleve seu negócio",
+  },
+  experienceSection: {
+    yearsAmount: "02+",
+    yearsPresentation: "Anos de Experiência na área",
+    experienceFields: [
+      "Criação de Sites",
+      "Aplicações com IA",
+      "Lojas Virtuais",
+    ],
+  },
+  portfolioSection: {
+    title: ["Trabalhos", "Recentes"],
+  },
+  curriculumSection: {
+    title: ["Experiência", "&", "Habilidades"],
+    decoration: "Software",
+    skillsSectionTitle: "Habilidades",
+    educationSectionTitle: "Formação",
+    experienceSectionTitle: "Experiência",
+    toolsSectionTitle: "Tecnologias",
+  },
+  skills: [
+    "Frontend",
+    "Backend",
+    "Mobile",
+    "Wordpress",
+    "WooComerce",
+    "Inteligência Artificial",
+  ],
+  skillsSection: {
+    postSkillsText: "& mais...",
+  },
+};
+
+export default strings;
