cad84f87339e18421d189d67bcf16b0520e9f165
lnobrz <lucasnobre37@outlook.com>
2024-01-11 00:35:49 -0300

    chore: improve render logic code quality in ProjectDetails

--- components/portfolio/portfolio-details/ProjectDetails.tsx
+++ components/portfolio/portfolio-details/ProjectDetails.tsx
@@ -1,28 +1,28 @@
 import { Item } from "@/data/portfolio";
 
 const ProjectDetails = ({ details }: { details: Item }) => {
-  if (!details || !details.projectInfo) {
-    return null;
-  }
-
   return (
     <>
-      <div className="col-12 mb-35">
-        <div className="pt-title fw-bold tx-dark text-uppercase">Date</div>
-        <div className="pt-text">{details.projectInfo.date}</div>
-      </div>
-      <div className="col-12 mb-35">
-        <div className="pt-title fw-bold tx-dark text-uppercase">
-          Client Name
-        </div>
-        <div className="pt-text">{details.projectInfo.clientName}</div>
-      </div>
-      <div className="col-12 mb-35">
-        <div className="pt-title fw-bold tx-dark text-uppercase">
-          Project Type
-        </div>
-        <div className="pt-text">{details.projectInfo.projectType}</div>
-      </div>
+      {details && details.projectInfo && (
+        <>
+          <div className="col-12 mb-35">
+            <div className="pt-title fw-bold tx-dark text-uppercase">Date</div>
+            <div className="pt-text">{details.projectInfo.date}</div>
+          </div>
+          <div className="col-12 mb-35">
+            <div className="pt-title fw-bold tx-dark text-uppercase">
+              Client Name
+            </div>
+            <div className="pt-text">{details.projectInfo.clientName}</div>
+          </div>
+          <div className="col-12 mb-35">
+            <div className="pt-title fw-bold tx-dark text-uppercase">
+              Project Type
+            </div>
+            <div className="pt-text">{details.projectInfo.projectType}</div>
+          </div>
+        </>
+      )}
     </>
   );
 };
