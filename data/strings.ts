399eb1ef77046b3fc21d2659a913373bcc5a87ab
Nobre <jose.nobre@hp.com>
2024-01-15 18:11:44 -0300

    feat: add new strings

--- data/strings.ts
+++ data/strings.ts
@@ -43,6 +43,55 @@ const strings = {
   skillsSection: {
     postSkillsText: "& mais...",
   },
+  experiencesSection: {
+    title:
+      "Experiência de 2 anos em desenvolvimento: especialista em websites, apps móveis e IA.",
+    experiences: [
+      {
+        role: "Desenvolvedor Fullstack",
+        company: "MyClients - Connecticut, USA",
+        workPeriod: "(05/2023 - Agora)",
+      },
+      {
+        role: "Desenvolvedor Frontend",
+        company: "Instituto Atlântico - Fortaleza, CE",
+        workPeriod: "(03/2022 - Agora)",
+      },
+    ],
+  },
+  educations: [
+    {
+      degree: "Análise e Desenvolvimento de Sistemas",
+      university: "UniFanor Wyden",
+    },
+    {
+      degree: "Engenharia de Software",
+      university: "Faculdade Descomplica",
+    },
+  ],
+  tools: [
+    "HTML",
+    "CSS",
+    "Javascript",
+    "Typescript",
+    "React",
+    "Next.js",
+    "SASS",
+    "Styled Components",
+    "Tailwind CSS",
+    "Material UI",
+    "Chakra UI",
+    "Strapi",
+    "Wordpress",
+    "Woocomerce",
+    "Node.js",
+    "Nest.js",
+    "Express",
+    "MongoDB",
+    "MYSQL",
+    "REST APIs",
+    "Figma",
+  ],
 };
 
 export default strings;
