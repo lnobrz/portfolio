b858a2bb09bccfdbea55382ccf29ab6507b0f485
Nobre <jose.nobre@hp.com>
2024-01-15 18:12:40 -0300

    chore: add new strings to education container

--- components/home-page/Education.tsx
+++ components/home-page/Education.tsx
@@ -1,16 +1,10 @@
+import strings from "@/data/strings";
 import React from "react";
 
-type Props = {
-  items: {
-    degree: string;
-    university: string;
-  }[];
-};
-
-const Education = ({ items }: Props) => {
+const Education = () => {
   return (
     <>
-      {items.map((item, i) => (
+      {strings.educations.map((item, i) => (
         <React.Fragment key={i}>
           <p className="text-lg tx-dark">{item.degree}</p>
           <div className="tx-sp2 text-uppercase">{item.university}</div>
