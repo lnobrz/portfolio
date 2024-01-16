3302b7814abfbadc5d1bd28dc01d908a29acfc5b
lnobrz <lucasnobre37@outlook.com>
2024-01-15 21:06:37 -0300

    feat: make Counter component swipeable

--- components/home-page/Counter/Counter.tsx
+++ components/home-page/Counter/Counter.tsx
@@ -1,57 +1,52 @@
 import React from "react";
-import { useInView } from "react-intersection-observer";
-import CountUp from "react-countup";
 import Image from "next/image";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { Navigation, Pagination } from "swiper/modules";
 
 type Props = {
   counters: {
     logo: string;
     title: string;
-    percentage: number;
   }[];
 };
 
 const Counter = ({ counters }: Props) => {
-  const { ref, inView } = useInView({
-    threshold: 0.5,
-    triggerOnce: true,
-  });
-
   return (
-    <>
-      {counters.map((counter, index) => (
-        <div key={index} className="col-lg-3 col-sm-6">
-          <div
-            className="counter-block-one mt-40 text-center"
-            ref={ref}
-            data-aos="fade-up"
-            data-aos-delay={`${100 * index}`}
-          >
-            <div className="icon-bg">
-              <div className="icon-img d-flex justify-content-center">
-                <Image
-                  src={counter.logo}
-                  alt="shape"
-                  className="lazy-img"
-                  width={99}
-                  height={99}
-                />
-              </div>
-              <div className="icon-title text-uppercase fs-18 tx-dark mt-35 lg-mt-30">
-                {counter.title}
+    <div className="h-25 d-flex overflow-x-auto">
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={4.5}
+        modules={[Navigation, Pagination]}
+        navigation
+        pagination={{ clickable: true }}
+      >
+        {counters.map((counter, index) => (
+          <SwiperSlide key={index} className="col-lg-3 col-sm-6">
+            <div
+              className="counter-block-one mt-40 text-center"
+              data-aos="fade-up"
+              data-aos-delay={`${100 * index}`}
+            >
+              <div className="icon-bg">
+                <div className="icon-img d-flex justify-content-center">
+                  <Image
+                    src={counter.logo}
+                    alt="shape"
+                    className="lazy-img"
+                    width={99}
+                    height={99}
+                  />
+                </div>
+                <div className="icon-title text-uppercase fs-18 tx-dark mt-35 lg-mt-30">
+                  {counter.title}
+                </div>
               </div>
             </div>
-            <div className="main-count font-recoleta fw-light tx-dark mt-25 lg-mt-20">
-              {inView ? (
-                <CountUp end={counter.percentage} suffix="%" />
-              ) : (
-                <span>0%</span>
-              )}
-            </div>
-          </div>
-        </div>
-      ))}
-    </>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   );
 };
 
--- components/home-page/Counter/CounterContainer.tsx
+++ components/home-page/Counter/CounterContainer.tsx
@@ -3,24 +3,76 @@ import Counter from "./Counter";
 const CounterContainer = () => {
   const counters = [
     {
-      logo: "/images/logo/Plogo-14.png",
+      logo: "/images/logo/wordpress.svg",
+      title: "Wordpress",
+    },
+    {
+      logo: "/images/logo/woocomerce.svg",
+      title: "WooComerce",
+    },
+    {
+      logo: "/images/logo/typescript.svg",
+      title: "Typescript",
+    },
+    {
+      logo: "/images/logo/react.svg",
+      title: "React",
+    },
+    {
+      logo: "/images/logo/next.svg",
+      title: "Next.js",
+    },
+    {
+      logo: "/images/logo/node.svg",
+      title: "Node.js",
+    },
+    {
+      logo: "/images/logo/nest.svg",
+      title: "Nest.js",
+    },
+    {
+      logo: "/images/logo/mongodb.svg",
+      title: "MongoDB",
+    },
+    {
+      logo: "/images/logo/mysql.svg",
+      title: "MySQL",
+    },
+    {
+      logo: "/images/logo/html.svg",
+      title: "HTML",
+    },
+    {
+      logo: "/images/logo/css.svg",
+      title: "CSS",
+    },
+    {
+      logo: "/images/logo/sass.svg",
+      title: "SASS",
+    },
+    {
+      logo: "/images/logo/styled-components.svg",
+      title: "Styled Components",
+    },
+    {
+      logo: "/images/logo/material-ui.svg",
+      title: "Material UI",
+    },
+    {
+      logo: "/images/logo/tailwind.svg",
+      title: "Tailwind CSS",
+    },
+    {
+      logo: "/images/logo/bootstrap.svg",
+      title: "Bootstrap",
+    },
+    {
+      logo: "/images/logo/express.svg",
+      title: "Express",
+    },
+    {
+      logo: "/images/logo/figma.svg",
       title: "Figma",
-      percentage: 93,
-    },
-    {
-      logo: "/images/logo/Plogo-15.png",
-      title: "Photoshop",
-      percentage: 92,
-    },
-    {
-      logo: "/images/logo/Plogo-16.png",
-      title: "Sketch",
-      percentage: 85,
-    },
-    {
-      logo: "/images/logo/Plogo-17.png",
-      title: "XD",
-      percentage: 82,
     },
   ];
 
