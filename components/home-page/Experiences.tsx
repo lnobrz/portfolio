6b1ff6026f88d08d690e3bf0022e126a3b4d7747
lnobrz <lucasnobre37@outlook.com>
2024-01-24 21:30:25 -0300

    chore: add missing key property

--- components/home-page/Experiences.tsx
+++ components/home-page/Experiences.tsx
@@ -1,4 +1,5 @@
 import strings from "@/data/strings";
+import React from "react";
 
 const Experiences = () => {
   return (
@@ -6,13 +7,13 @@ const Experiences = () => {
       <p className="text-xxl tx-dark mb-75 lg-mb-30">
         {strings.experiencesSection.title}
       </p>
-      {strings.experiencesSection.experiences.map((experience) => {
+      {strings.experiencesSection.experiences.map((experience, index) => {
         return (
-          <>
+          <React.Fragment key={`experience-${index}`}>
             <div className="mb-1 tx-dark tx-sp1">{experience.role}</div>
             <div className="mb-1 fs-20 opacity-50">{experience.company}</div>
             <div className="mb-2 fs-20 opacity-50">{experience.workPeriod}</div>
-          </>
+          </React.Fragment>
         );
       })}
     </>
